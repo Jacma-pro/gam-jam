8651fd0897bf6a9f64118ceb8af961a9564d7adc
Dorian Jacolin <dorian.jacolin@my-digital-school.org>
2026-02-02 11:04:29 +0100

    add : mouvement

--- scenes/fire_player/fire_player.gd
+++ scenes/fire_player/fire_player.gd
@@ -1,26 +1,31 @@
 extends CharacterBody2D
 
+@export_category("Mouvement")
+@export var speed = 400.0
+@export var jump_velocity = -600.0
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-const GRAVITY = 0
+@export_category("Controles")
+@export var action_left = "p1_left"
+@export var action_right = "p1_right"
+@export var action_jump = "p1_up"
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed(action_jump) and is_on_floor():
+		velocity.y = jump_velocity
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis(action_left, action_right)
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
