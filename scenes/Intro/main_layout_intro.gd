2bc23caedba62dba545874aafb5b56a1d11e5cb3
aant-code <aant444444@gmail.com>
2026-02-03 13:39:33 +0100

    menu pause, intro, background

--- scenes/Intro/main_layout_intro.gd
+++ scenes/Intro/main_layout_intro.gd
@@ -1,12 +1,23 @@
 extends Control
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+const PAUSE_MENU_SCENE = preload("res://scenes/Menu/pause_menu.tscn")
 
 func _ready() -> void:
 	# Met le jeu en pause pour figer les joueurs
 	get_tree().paused = true
 	# Connecte le signal de fin d'animation pour relancer le jeu
 	animation_player.animation_finished.connect(_on_animation_finished)
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("pause"):
+		if not get_tree().paused:
+			pause_game()
+
+func pause_game() -> void:
+	var pause_menu = PAUSE_MENU_SCENE.instantiate()
+	add_child(pause_menu)
+	get_tree().paused = true
 
 func _on_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "intro":
